a053fa63984f899120a3cd5fb53b63f9e13d49f0
Steve Kelly <176453175+phishyish@users.noreply.github.com>
2025-02-10 17:09:04 -0600

    Making changes to Odin Project.

---
+++ javascript-testing/javascript.js
@@ -0,0 +1,24 @@
+// let firstname = 'John';
+// let surname = 'Doe';
+
+// console.log(firstname);
+// console.log(surname);
+
+
+// const pi =3.14;
+
+// console.log((4 + 6 + 9) / 77);
+
+
+// let a = 10;
+// console.log(a*9);
+
+// let b = 7*a;
+// console.log(b);
+
+
+let max = 57;
+let actual = max - 13;
+let percentage = actual/max;
+console.log(percentage);
+
